7572552224e18948ba031b75f7ff8765f48990e7
liamaharon <liam.aharon@hotmail.com>
2020-05-04 16:03:53 +1000

    Periodically send transactions

--- index.js
+++ index.js
@@ -19,11 +19,24 @@ async function main() {
     // Build Docker image
     console.log('--- BUILDING DOCKER IMAGE ---')
     execSync(`${__dirname}/s/build`, { stdio: 'inherit' })
+    console.log('Built')
 
     // Run Docker image
     console.log('--- STARTING NODES ---')
     execSync(`${__dirname}/s/run`)
-    console.log('Nodes running!')
+    await new Promise(r => setTimeout(r, 10000))
+    console.log('Running')
+
+    // Fund fake user accounts
+    console.log('--- FUNDING FAKE USERS ---')
+    await fundUsers()
+    console.log('Funded')
+
+    // Start sending random transactions
+    randomTransaction()
+
+    // Start mining blocks
+    mine()
 
     // eslint-disable-next-line no-constant-condition
     while (true) {
@@ -32,11 +45,56 @@ async function main() {
     }
   } catch (error) {
     console.error('Unexpected error starting nodes')
-    console.error(error)
+    console.error(error.toString())
   }
 }
 
 main()
+
+function randomNumber(min, max) {
+  return Math.random() * (max - min) + min
+}
+
+function randomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+async function fundUsers() {
+  const nUsers = 9
+  for (let i = 0; i <= nUsers; i += 1) {
+    // Mines a block for each user, giving them some btc
+    const address = execSync(`${__dirname}/cli/user${i} -regtest getnewaddress`).toString()
+    execSync(`${__dirname}/cli/user${i} -regtest generatetoaddress 101 ${address}`)
+    console.log(`User ${i} funded`)
+  }
+}
+
+async function randomTransaction() {
+  // Wait between 0.05-1s
+  await new Promise(r => setTimeout(r, randomNumber(50, 1000)))
+
+  // Choose sender, receiver, amt to send
+  const sender = randomInteger(0, 9)
+  const receiver = randomInteger(0, 9)
+  const amount = randomNumber(0.00001, 0.01).toFixed(5)
+
+  // Generate receive addr
+  const toAddress = execSync(`${__dirname}/cli/user${receiver} -regtest getnewaddress`).toString()
+
+  // Send to receiver
+  const txId = execSync(`${__dirname}/cli/user${sender} sendtoaddress "${toAddress}" ${amount}`)
+    .toString()
+    .replace(/(\r\n|\n|\r)/gm, '') // remove training newline
+
+  console.log(`New pending tx ${txId}`)
+  randomTransaction()
+}
+
+function mine() {
+  // const miner = randomInteger(0, 9)
+  // const mempoolinfo = execSync(`${__dirname}/cli/user${miner} help getmempoolinfo`).toString()
+  // console.log(mempoolinfo)
+}
 
 function stopNodes() {
   console.log('\nShutting down regtest nodes..')
